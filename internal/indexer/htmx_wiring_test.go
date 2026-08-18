9fc24761b137f30b007e167aa95ce5b6aab2ed25
Stephen Eaton <seaton@strobotics.com.au>
2026-08-18 20:00:00 +1000

    feat(indexer): register HtmxExtractor for html/gotmpl/htmldjango

---
+++ internal/indexer/htmx_wiring_test.go
@@ -0,0 +1,24 @@
+package indexer
+
+import (
+	"testing"
+
+	"github.com/zzet/gortex/internal/contracts"
+)
+
+func TestBuildPerFileContractExtractors_IncludesHtmx(t *testing.T) {
+	idx := &Indexer{}
+	_, byLang := idx.buildPerFileContractExtractors()
+	for _, lang := range []string{"html", "gotmpl", "htmldjango"} {
+		found := false
+		for _, ex := range byLang[lang] {
+			if _, ok := ex.(*contracts.HtmxExtractor); ok {
+				found = true
+				break
+			}
+		}
+		if !found {
+			t.Fatalf("byLang[%q] has no HtmxExtractor", lang)
+		}
+	}
+}
